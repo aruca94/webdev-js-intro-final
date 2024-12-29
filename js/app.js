641f9012a91c4f153da791012d75805b5b0bde20
Josh <aftershock840@gmail.com>
2024-12-29 10:24:40 -0500

    very basic functionality added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,54 @@
 "use strict";
+
+// constants for element tags below
+// need constants for:
+// count input box  TODO: use .value on this to display #
+const numberSelectorEl = document.getElementById("guess-input");
+// result
+const resultOutputEl = document.getElementById("current-guess");
+// your guess
+const currentGuessEl = document.getElementById("current-guess");
+// computer guess
+const ComputerGuessEl = document.getElementById("computer-guess");
+// guess history
+const guessHistoryEl = document.getElementById("guess-history");
+// restart button
+const restartBtnEl = document.getElementById("restart-btn");
+//submit button
+const submitBtnEl = document.getElementById("submit-btn");
+
+// if there's enough time, fade background and hide display of everything except restart button, change text, and ask player if they want to restart
+
+// mutable variables below
+
+//functions below
+function userNumSelect() {
+    resultOutputEl.innerText = numberSelectorEl.value;
+};
+
+function computerNumber() {
+    let randomNum = Math.random() * (1,9) + 1;
+    ComputerGuessEl.innerText = Math.round(randomNum);
+};
+
+//render function
+function submitRender() {
+    userNumSelect();
+    computerNumber();
+};
+
+function restartRender() {
+
+};
+//eventlistener1 below
+submitBtnEl.addEventListener("click", function() {
+    submitRender();
+});
+//eventlistener2 below
+restartBtnEl.addEventListener("click", function() {
+    restartRender();
+});
+
+
+// user will select a number and hit the submit button. Then, result, both guesses will populate. guess history will show all attempts in the game. Once 3 attempts have been made, if user is unsuccessful the game will restart.
+
