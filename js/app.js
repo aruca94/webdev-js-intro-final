199e0affb3266862439816326891c707b3a99a0c
Josh <aftershock840@gmail.com>
2025-01-05 11:26:30 -0500

    cleaned up functions

--- js/app.js
+++ js/app.js
@@ -9,19 +9,20 @@ const resultOutputEl = document.getElementById("guess-message");
 // your guess
 const currentGuessEl = document.getElementById("current-guess");
 // computer guess
-const ComputerGuessEl = document.getElementById("computer-guess");
+const computerGuessEl = document.getElementById("computer-guess");
 // guess history
 const guessHistoryEl = document.getElementById("guess-history");
 // restart button
 const restartBtnEl = document.getElementById("restart-btn");
 //submit button
 const submitBtnEl = document.getElementById("submit-btn");
+const attemptsLeftEl = document.getElementById("attempts-left");
 
 // if there's enough time, fade background and hide display of everything except restart button, change text, and ask player if they want to restart
 
 // mutable variables below
 let historyArr = [];
-let userAttempt = 0
+let userAttempt = 3;
 
 //functions below
 function userNumSelect() {
@@ -30,32 +31,34 @@ function userNumSelect() {
 
 function computerNumber() {
     let randomNum = Math.random() * (1,9) + 1;
-    ComputerGuessEl.innerText = Math.round(randomNum);
+    computerGuessEl.innerText = Math.round(randomNum);
 };
 
-function results() {  // returns wrong when comp. # is 10
-    if(currentGuessEl.innerText > ComputerGuessEl.innerText ) {
-        return resultOutputEl.innerText = "too high";
-        }else if(currentGuessEl.innerText < ComputerGuessEl.innerText) {
-        return resultOutputEl.innerText = "too low";
+function results() {
+
+
+    if(parseInt(currentGuessEl.innerText) > parseInt(computerGuessEl.innerText) && userAttempt > 1 ) {
+         resultOutputEl.innerText = "too high";
+         attemptsLeftEl.innerText = `You have ${userAttempt - 1} attempts left.`;
+        }else if(parseInt(currentGuessEl.innerText) < parseInt(computerGuessEl.innerText) && userAttempt > 1) {
+        resultOutputEl.innerText = "too low";
+        attemptsLeftEl.innerText = `You have ${userAttempt - 1} attempts left.`;
+        }else if(parseInt(currentGuessEl.innerText) === parseInt(computerGuessEl.innerText) && userAttempt >= 1){
+         resultOutputEl.innerText = "You win!";
+         submitBtnEl.disabled = true;
+         attemptsLeftEl.innerText = `Would you like to try again?`;
         }else{
-        return resultOutputEl.innerText = "correct!";
+            submitBtnEl.disabled = true;
+            resultOutputEl.innerText = "You have lost";
+            attemptsLeftEl.innerText = `Would you like to try again?`;
         };
-};
-function guessHistoryDisplay(event) {
+    };
+function guessHistoryDisplay() {
         let userNumber = numberSelectorEl.value;
         historyArr.push(userNumber);
-        return guessHistoryEl.innerText = historyArr;
-};
-
-function gameOver () {
-    if(userAttempt === 3){
-        //reset counter to 0
-        //display some end game message
-        // enable restart button
-        //once user restarts game
+        guessHistoryEl.innerText = historyArr;
     };
-};
+
 
 //render function
 function submitRender() {
@@ -64,6 +67,7 @@ function submitRender() {
     computerNumber();
     results();
     guessHistoryDisplay()
+    // triesLeft();
 };
 
 function restartRender() {
@@ -72,18 +76,18 @@ function restartRender() {
 //eventlistener1 below
 submitBtnEl.addEventListener("click", function(event) {
     // logic that checks for an acceptable input
-    if(numberSelectorEl.textLength === 0 || numberSelectorEl.value <= 0) {
+    if( numberSelectorEl.value <= 0 || numberSelectorEl.value > 10) {
         alert(`please input a number 1-10`);
         event.preventDefault();
     }else{
-    submitRender();
+        submitRender();
+        userAttempt -=1;
     };
 });
 //eventlistener2 below
-restartBtnEl.addEventListener("click", function() {
-    restartRender();
-});
+// restartBtnEl.addEventListener("click", function() {
+//     restartRender();
+// });
 
 
 // user will select a number and hit the submit button. Then, result, both guesses will populate. guess history will show all attempts in the game. Once 3 attempts have been made, if user is unsuccessful the game will restart.
-
